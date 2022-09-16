cbab0bd7c255555e62342ead17dc5d14a0c8376c
Várszegi Ádám <varszegi.adam75@gmail.com>
2022-09-16 16:28:52 +0200

    Main kiegészítés

--- src/OOP_alapok/Pont/pont/Pont.java
+++ src/OOP_alapok/Pont/pont/Pont.java
@@ -13,6 +13,14 @@ public class Pont {
         this.x=x;
         this.y=y;
     }
+    public Pont(int n){
+        this.x= koordinataGeneralasa(n);
+        this.y= koordinataGeneralasa(n);
+    }
+
+    private int koordinataGeneralasa(int n) {
+        return (int) (Math.random() * (2 * n + 1)) - n;
+    }
 
     @Override
     public String toString() {
