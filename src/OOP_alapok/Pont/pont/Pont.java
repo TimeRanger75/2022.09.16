b98d492783537b992f06ee2489f6cf846dc5f47a
Várszegi Ádám <varszegi.adam75@gmail.com>
2022-09-16 16:08:24 +0200

    Pont osztály

---
+++ src/OOP_alapok/Pont/pont/Pont.java
@@ -0,0 +1,16 @@
+package OOP_alapok.Pont.pont;
+
+public class Pont {
+    private  int x;
+    private int y;
+
+    public  Pont(){
+        x=0;
+        this.y=0;
+    }
+
+    public  Pont(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+}
